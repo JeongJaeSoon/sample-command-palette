ffef37e5f6130b046a74e837c32a3dcb2094beda
JeongJaeSoon <94jaesoon.jeong@gmail.com>
2025-02-23 00:24:16 +0900

    feat: Add base route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { About, Home, Users } from "./page";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header style={{ backgroundColor: "lightgray", padding: "20px", textAlign: "center"}}>
+        <ul style={{ display: "flex", gap: "4rem", listStyle: "none", textAlign: "center", margin: "0 auto", width: "fit-content" }}>
+          <li style={{width: "6rem"}}><Link to="/">Home</Link></li>
+          <li style={{width: "6rem"}}><Link to="/about">About</Link></li>
+          <li style={{width: "6rem" }}><Link to="/users">Users</Link></li>
+        </ul>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
